a6c4d6669eebd2cfbafeeafe4cc89f6d7643cd88
adiko <dikodam@googlemail.com>
2017-04-25 14:11:06 +0200

    Fraction implemented

--- Warenkorb/Fraction.cs
+++ Warenkorb/Fraction.cs
@@ -1,16 +1,20 @@
 ﻿using System;
+using System.Runtime.Remoting.Messaging;
 
 namespace Warenkorb
 {
     public class Fraction : IComparable
     {
-        private double Numerator { get; set; }
-        private double Denominator { get; set; }
+        /// Zähler
+        private int Numerator { get; set; }
+
+        /// Nenner
+        private int Denominator { get; set; }
 
         /// <summary>
         ///
         /// </summary>
-        /// <param name="o"></param>
+        /// <param name="o">Objekt, mit dem verglichen wird</param>
         /// <returns>
         /// -1 für this < obj <br/>
         /// 0 für this == obj <br/>
@@ -20,9 +24,21 @@ namespace Warenkorb
         {
             if (!(o is Fraction))
             {
-                
+                throw new Exception("wrong type!");
             }
 
+            Fraction compared = (o as Fraction);
+
+            if (this.Numerator == compared.Numerator && this.Denominator == compared.Denominator)
+            {
+                return 0;
+            }
+            if (this.Numerator * compared.Denominator < compared.Numerator * this.Denominator)
+            {
+                return -1;
+            }
+            // else
+            return 1;
         }
     }
 }
